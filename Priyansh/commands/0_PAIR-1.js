5b2a6f134b4695781391511cdec4242b8d3f9f6d
brokenboys <shrivastavaabhishek687@gmail.com>
2025-06-11 20:58:59 +0530

    Update 0_PAIR-1.js

--- Priyansh/commands/0_PAIR-1.js
+++ Priyansh/commands/0_PAIR-1.js
@@ -41,7 +41,7 @@ async function makeImage({ one, two }) {
 
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    pairing_img.composite(circleOne.resize(300, 150), 300, 300).composite(circleTwo.resize(900, 150), 300, 300);
+    pairing_img.composite(circleOne.resize(300, 300), 100, 100).composite(circleTwo.resize(300, 300), 600, 100);
 
     let raw = await pairing_img.getBufferAsync("image/png");
 
