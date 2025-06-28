768612358535a7df0b898bf7b041910cac72d145
samarkhanji <knsamar513@gmail.com>
2025-06-28 10:01:46 +0530

    Update 0_AADI-JALEBI.js

--- Priyansh/commands/0_AADI-JALEBI.js
+++ Priyansh/commands/0_AADI-JALEBI.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("jalebi")==0 || event.body.indexOf("Jalebi")==0 || event.body.indexOf("JALEBI")==0 || event.body.indexOf("JABELI")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐉𝐚𝐥𝐞𝐁𝐢\n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐔𝐇𝐄𝐁 𝐊𝐇𝐀𝐍 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐉𝐚𝐥𝐞𝐁𝐢\n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/b9ad6b8258591ea5d1e7d13773d1e222.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
