ed1c7eb748f9bf9dc7dd4b56ae27c9717f9aa04e
samarkhanji <knsamar513@gmail.com>
2025-06-28 10:25:17 +0530

    Update 0_AADI_GOLGAPPE.js

--- Priyansh/commands/0_AADI_GOLGAPPE.js
+++ Priyansh/commands/0_AADI_GOLGAPPE.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Golgappe")==0 || event.body.indexOf("Golgappa")==0 || event.body.indexOf("Pani puri")==0 || event.body.indexOf("PANI PURI")==0) {
 		var msg = {
-				body: "𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n 🩷 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 🩷\n\n< ────────────────── >",
+				body: "𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐔𝐇𝐄𝐁 𝐊𝐇𝐀𝐍 \n 🩷 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 🩷\n\n< ────────────────── >",
 				attachment: fs.createReadStream(__dirname + `/noprefix/Pin_on_Pins_by_you(360P)(1).mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
