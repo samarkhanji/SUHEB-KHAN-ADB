74cd0c428fceed05cd56dafcc26883b6a0105375
samarkhanji <knsamar513@gmail.com>
2025-06-28 10:15:16 +0530

    Update 0_AADI-PIZZA.js

--- Priyansh/commands/0_AADI-PIZZA.js
+++ Priyansh/commands/0_AADI-PIZZA.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("pizza")==0 || event.body.indexOf("Pizza")==0 || event.body.indexOf("PIZZA")==0 || event.body.indexOf("PIZZAA")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐈𝐙𝐙𝐀 \n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐬𝐮𝐡𝐞𝐛 𝐤𝐡𝐚𝐧 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐈𝐙𝐙𝐀 \n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Pinterest-Abhijeet-Agnihotri-pizza.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
