bd369603e3e6a2c3b19bf4cd1b06d643cea076d6
samarkhanji <knsamar513@gmail.com>
2025-06-28 09:59:13 +0530

    Update 0_AADI-DUDH.js

--- Priyansh/commands/0_AADI-DUDH.js
+++ Priyansh/commands/0_AADI-DUDH.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("dudh")==0 || event.body.indexOf("DUDH")==0 || event.body.indexOf("DHUD")==0 || event.body.indexOf("Dudh")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐃𝐮𝐝𝐇 \n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐮𝐡𝐞𝐛 𝐊𝐇𝐀𝐍  \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐃𝐮𝐝𝐇 \n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/(720p).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
