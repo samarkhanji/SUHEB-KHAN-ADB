b1d736112ed85abfa8fe349d42f236de04cb2570
samarkhanji <knsamar513@gmail.com>
2025-06-28 10:21:11 +0530

    Update 0_AADI-SAMOSE.js

--- Priyansh/commands/0_AADI-SAMOSE.js
+++ Priyansh/commands/0_AADI-SAMOSE.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("Samose")==0 || event.body.indexOf("samosa")==0 || event.body.indexOf("SAMOSE")==0 || event.body.indexOf("SAMOSA")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐒𝐚𝐦𝐨𝐒𝐞\n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐮𝐡𝐞𝐛 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐒𝐚𝐦𝐨𝐒𝐞\n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/samosas_lover_here_|_Ricette,_Easy,_Crockpot(360P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
