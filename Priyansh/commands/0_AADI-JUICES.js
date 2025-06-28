30044e5cde94de05580adc5b6fd6649c1765e1de
samarkhanji <knsamar513@gmail.com>
2025-06-28 10:02:29 +0530

    Update 0_AADI-JUICES.js

--- Priyansh/commands/0_AADI-JUICES.js
+++ Priyansh/commands/0_AADI-JUICES.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("juice")==0 || event.body.indexOf("juices")==0 || event.body.indexOf("Juice")==0 || event.body.indexOf("JUICE")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐉𝐮𝐢𝐂𝐞\n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐔𝐇𝐄𝐁 𝐊𝐇𝐀𝐍 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐉𝐮𝐢𝐂𝐞\n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Never_forget_to_hydrate_your_body_every_day🥤🍓😋_|_Gıda,_Smoothies,_Çilekler(480P).mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
