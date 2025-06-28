d8f87323d77e9257e25d136f67d3994ea3134136
samarkhanji <knsamar513@gmail.com>
2025-06-28 10:04:59 +0530

    Update 0_AADI-LASSI.js

--- Priyansh/commands/0_AADI-LASSI.js
+++ Priyansh/commands/0_AADI-LASSI.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("lassi")==0 || event.body.indexOf("Lassi")==0 || event.body.indexOf("LASSI")==0 || event.body.indexOf("LASSEE")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐋𝐚𝐒𝐒𝐢\n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐮𝐡𝐞𝐛 𝐊𝐇𝐀𝐍  \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐋𝐚𝐒𝐒𝐢\n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/d190b09072f95cf62eda8634c130be8c.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
