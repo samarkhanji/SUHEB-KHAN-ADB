cee34b6553bd1e89389a1432c4be4c1fa2b95528
samarkhanji <knsamar513@gmail.com>
2025-06-28 09:57:25 +0530

    Update 0_AADI-CHICKEN.js

--- Priyansh/commands/0_AADI-CHICKEN.js
+++ Priyansh/commands/0_AADI-CHICKEN.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("chicken")==0 || event.body.indexOf("Chicken")==0 || event.body.indexOf("CHICKEN")==0 || event.body.indexOf("CHIKAN")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐡𝐢𝐜𝐇𝐞𝐍\n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐔𝐇𝐄𝐁 𝐊𝐇𝐀𝐍 𝐊 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐡𝐢𝐜𝐇𝐞𝐍\n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/62306739ed186731b1f646029335e853.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
