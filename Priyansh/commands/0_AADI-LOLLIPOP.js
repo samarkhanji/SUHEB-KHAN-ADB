345649396da7b1b923dc4da8bd965608e26d7113
samarkhanji <knsamar513@gmail.com>
2025-06-28 10:05:47 +0530

    Update 0_AADI-LOLLIPOP.js

--- Priyansh/commands/0_AADI-LOLLIPOP.js
+++ Priyansh/commands/0_AADI-LOLLIPOP.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("lolipop")==0 || event.body.indexOf("LOLIPOP")==0 || event.body.indexOf("Lolipop ")==0 || event.body.indexOf("lolipopp")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐋𝐎𝐋𝐈𝐏𝐎𝐏\n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐬𝐮𝐡𝐞𝐛 𝐤𝐡𝐚𝐧 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐋𝐎𝐋𝐈𝐏𝐎𝐏\n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/8ae90a3cec3e329941aa87a4c6cacb34.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
